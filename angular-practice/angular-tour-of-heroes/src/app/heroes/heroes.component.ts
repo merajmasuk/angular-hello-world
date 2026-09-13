6b0fc00baadc71334e6d07cd863f1f8982eec9ee
Meraj al Maksud <merajmasuk@gmail.com>
2026-09-13 21:57:14 +0600

    hello world and tour of heroes

---
+++ angular-practice/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrl: './heroes.component.css'
+})
+export class HeroesComponent {
+  heroes: Hero[] = [];
+
+  constructor(
+    private heroService: HeroService
+  ) {}
+
+  selectedHero?: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
+  }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+}
